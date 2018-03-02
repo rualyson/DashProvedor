037ae37526d40896cbdc60fab46c7b9acc960bf1
Rualyson Cavalcante <rualyson.cavalcante@dce.ufpb.br>
2018-03-02 09:31:23 -0300

    Initial commit

---
+++ app/src/main/java/rualyson/com/dashprovedor/MainActivity.java
@@ -0,0 +1,27 @@
+package rualyson.com.dashprovedor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        Button botao1 = (Button) findViewById(R.id.botao1);
+
+        botao1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                setContentView(R.layout.activity_main);
+            }
+        });
+
+    }
+    public void login(View view){
+        setContentView(R.layout.activity_main);
+    }
+}
